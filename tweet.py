8ad64d3207cbb0bd9620794ce7dac65e7d3dc3a8
Lucas Hoke <lucas.a.hoke@gmail.com>
2024-02-03 23:36:19 -0700

    Fixed readability and other minor issues

--- tweet.py
+++ tweet.py
@@ -9,24 +9,24 @@ genius = lyricsgenius.Genius(os.environ['GENIUSAPIKEY'])
 all_songs = ["Age of Delilah", "A House In Nebraska", "A long, unfortunate while (demo)", "American Teenager", "Antlers", "Bruises", "Casings", "Crush", "Crying During Sex", "Dog Days", "Dust Bowl (Demo)", "Everytime - Spotify Singles", "Family Tree (Intro)", "Family Tree", "Gibson Girl", "God's Country", "Golden Age", "Growing Pains", "Half-Cocked", "Hard Times", "Head in the Wall", "Inbred", "Knuckle Velvet", "Lilies", "Michelle Pfeiffer", "Misuse Oh", "Ptolemaea", "Strangers", "Sun Bleached Flies", "Sunday Morning", "Thoroughfare", "Two-Headed Mother", "Unpunishable", "Western Nights", "Waco, Texas (Demo)"]
 
 # Determine a random song, return full lyrics string and song name
-def get_raw_lyrics():
+def find_lyrics():
     genius_client_access_token = (os.environ['GENIUSCAT'])
     # Create genius object
     genius = lyricsgenius.Genius(genius_client_access_token)
-    random_song_title = random.choice(all_songs)
-    lyrics = genius.search_song(random_song_title, "Ethel Cain").lyrics
-    song = random_song_title.upper()
+    random_song = random.choice(all_songs)
+    lyrics = genius.search_song(random_song, "Ethel Cain").lyrics
+    song = random_song.upper()
     return lyrics, song
 
 # Split full lyrics string 
-def get_tweet_from(lyrics, tweet):
+def write_tweet(lyrics, tweet):
     # Splits string into array
     lines = lyrics.split('\n')
     # Delimitator to remove miscellaneous characters
     for index in range(len(lines)):
         if lines[index] == "" or "[" in lines[index]:
-            lines[index] = "XXX"
-    lines = [i for i in lines if i != "XXX"]
+            lines[index] = "!DELETE!"
+    lines = [i for i in lines if i != "!DELETE!"]
     # Finds random two lines from lyrics
     random_num = random.randrange(0, len(lines)-1)
     tweet = lines[random_num] + "\n" + lines[random_num+1]
@@ -42,7 +42,7 @@ def get_tweet_from(lyrics, tweet):
 def handler(event, context):
     tweet =""
     api = tweepy.Client(bearer_token= os.environ['TBEARERTOKEN'], consumer_key=os.environ['TCONSUMERKEY'], consumer_secret=os.environ['TCONSUMERSECRET'], access_token=os.environ['TACCESSTOKEN'], access_token_secret=os.environ['TACCESSSECRET'], return_type=tweepy.Response, wait_on_rate_limit=False)
-    lyrics, song = get_raw_lyrics()
-    tweet = get_tweet_from(lyrics, tweet)
+    lyrics, song = find_lyrics()
+    tweet = write_tweet(lyrics, tweet)
     # Posts tweet
     status = api.create_tweet(text=tweet, place_id=song)
